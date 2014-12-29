d9666453e6fd926ac269c4ea0a538293869c6a88
Vishnu Atrai <me@vishnuatrai.com>
2014-12-30 00:21:24 +0530

    get teammembers for project

--- client/src/app/projectsinfo/projectsinfo.js
+++ client/src/app/projectsinfo/projectsinfo.js
@@ -16,7 +16,11 @@ angular.module('projectsinfo', [], ['$routeProvider', function($routeProvider){
         resolve:{
             project:['$route', 'Projects', function ($route, Projects) {
                 return Projects.getById($route.current.params.projectId);
+            }],
+            users:['$route', 'Users', function ($route, Users) {
+                return Users.all();
             }]
+
         }
 
       });
@@ -32,11 +36,31 @@ angular.module('projectsinfo').controller('ProjectsInfoListCtrl', ['$scope', '$l
 
 }]);
 
-angular.module('projectsinfo').controller('ProjectsShowCtrl', ['$scope', '$location', 'project', function($scope, $location, project) {
+angular.module('projectsinfo').controller('ProjectsShowCtrl', ['$scope', '$location', 'project', 'users', function($scope, $location, project, users) {
 
     $scope.project = project;
+    $scope.users = users;
+
+    //prepare users lookup, just keep references for easier lookup
+    $scope.usersLookup = {};
+    angular.forEach($scope.users, function (value, key) {
+        $scope.usersLookup[value.$id()] = value;
+    });
+
+    $scope.productOwnerFullName = function () {
+        return $scope.usersLookup[$scope.project.productOwner].getFullName();
+    };
+
+    $scope.scrumMasterFullName = function () {
+        return $scope.usersLookup[$scope.project.scrumMaster].getFullName();
+    };
+
     $scope.back = function(){
       return $location.url('/projectsinfo');
     };
+
+}])
+.controller('TeamMembersController', ['$scope', function($scope) {
+    $scope.project.teamMembers = $scope.project.teamMembers || [];
 
 }]);
--- client/src/common/resources/users.js
+++ client/src/common/resources/users.js
@@ -1,7 +1,7 @@
 angular.module('resources.users', []);
 angular.module('resources.users').factory('Users', function ($resource) {
 
-  var userResource = $resource('users');
+  var userResource = $resource('/users');
   userResource.prototype.getFullName = function () {
     return this.lastName + " " + this.firstName + " (" + this.email + ")";
   };
