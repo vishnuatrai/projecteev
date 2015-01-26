a9837e808e3057a081c653c8171de68e4220accd
Vishnu Atrai <me@vishnuatrai.com>
2015-01-26 23:58:18 +0530

    fix sqlize promises

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,86 +2,81 @@ var models = require('../models');
 exports.addRoutes = function (app) {
 
   app.get('/projectsinfo', function(req,res){
-      models.Project.findAll({ productOwner: req.user.id }, function(projects){
-          res.json(200, projects );
-      });
+    models.Project.findAll({ where: { productOwner: req.user.id } }).then(function(projects){
+      res.json(200, projects );
+    });
   });
 
   app.get('/projectsinfo/:projectId', function(req,res){
-      models.Project.byId(req.params.projectId, function(project){
+    models.Project.find({ where: { id: req.params.projectId }}).then(function(project){
       res.json(200, project);
     })
   });
 
   app.get('/projects/:projectId/productbacklog', function(req,res){
-      models.ProductBacklog.all({ projectId: req.params.projectId }, function(product_backlogs){
+    models.ProductBacklog.findAll({ where: { projectId: req.params.projectId }}).then(function(product_backlogs){
       res.json(200, product_backlogs);
     })
   });
 
   app.post('/projects/:projectId/productbacklog', function(req,res){
-    var params = req.body;
-    params['projectId'] = req.params.projectId;
-      models.ProductBacklog.createOrUpdate(params, function(product_backlog){
-      res.json(200, product_backlog);
-    })
+    models.ProductBacklog.findOrInitialize({ where: { id: req.body.id } })
+      .then(function(result){ result[0].updateAttributes( req.body ) })
+      .then(function(product_backlog){ res.json(200, product_backlog); })
   });
 
   app.get('/projects/:projectId/productbacklog/:productBacklogId', function(req,res){
-      models.ProductBacklog.byId(req.params.productBacklogId, function(product_backlog){
+    models.ProductBacklog.find({where: { id: req.params.productBacklogId } }).then(function(product_backlog){
       res.json(200, product_backlog);
     })
   });
 
   app.delete('/projects/:projectId/productbacklog/:productBacklogId', function(req,res){
-    ProductBacklog.delete(req.params.productBacklogId, function(){
+    models.ProductBacklog.destroy({where: { id: req.params.productBacklogId }}).then(function(result){
       res.json(200, {});
     })
   });
 
   app.get('/projects/:projectId/sprints', function(req,res){
-    Sprint.all({ projectId: req.params.projectId }, function(sprints){
+    models.Sprint.findAll({ where: { projectId: req.params.projectId } }).then(function(sprints){
       res.json(200, sprints);
     })
   });
 
   app.post('/projects/:projectId/sprints', function(req,res){
-    var params = req.body;
-    params['projectId'] = req.params.projectId;
-    Sprint.createOrUpdate(params, function(sprint){
-      res.json(200, sprint);
-    })
+    models.Sprint.findOrInitialize({ where: { id: req.body.id } })
+      .then(function(result){ result[0].updateAttributes( req.body ) })
+      .then(function(sprint){ res.json(200, sprint); })
   });
 
   app.get('/projects/:projectId/sprints/:sprintId', function(req,res){
-    Sprint.byId(req.params.sprintId, function(sprint){
+    models.Sprint.find({ where: { id: req.params.sprintId } }).then(function(sprint){
       res.json(200, sprint);
     })
   });
 
   app.delete('/projects/:projectId/sprints/:sprintId', function(req,res){
-    Sprint.delete(req.params.sprintId, function(){
+    models.Sprint.destroy({where: { id: req.params.sprintId}}).then(function(result){
       res.json(200, {});
     })
   });
 
   app.get('/projects/:projectId/sprints/:sprintId/tasks', function(req,res){
-    Task.all({ sprintId: req.params.sprintId }, function(tasks){
+      models.Task.findAll({ where: { sprintId: req.params.sprintId } }).then(function(tasks){
       res.json(200, tasks);
     })
   });
 
   app.get('/projects/:projectId/sprints/:sprintId/tasks/:taskId', function(req,res){
-    Task.byId(req.params.taskId, function(task){
+      models.Task.find({ where: { id: req.params.taskId } }).then(function(task){
       res.json(200, task);
     })
   });
 
   app.post('/projects/:projectId/sprints/:sprintId/tasks', function(req,res){
-    var params = req.body;
-    Task.createOrUpdate(params, function(task){
-      res.json(200, task);
-    })
+    models.Task.findOrInitialize({ where: { id: req.body.id } })
+      .then(function(result){ result[0].updateAttributes( req.body ) })
+      .then(function(task){ res.json(200, task); })
   });
 
 };
