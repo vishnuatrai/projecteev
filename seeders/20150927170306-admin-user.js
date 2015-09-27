c6efa8e11d6f395e4a3cd38a6f96fcf9f1c28045
Vishnu Sharma <vishnu_sharma2@optum.com>
2015-09-27 17:07:04 +0530

    init db moved to seeders

---
+++ seeders/20150927170306-admin-user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.Insert('User', [{
+      firstName: 'Vishnu Kumar', 
+      lastName: 'sharma', 
+      email: 'vishnu.sharma@gmail.com',
+      password: 'password', 
+      admin: true
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
